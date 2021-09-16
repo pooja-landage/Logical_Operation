79e52998a087cf77bce5ea44f346b00832aa681f
pooja-landage <poojalandage22@gmail.com>
2021-09-16 07:47:32 +0530

    Add project files.

---
+++ Pratice5Logical/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pratice5Logical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Logical Operator!");
+
+            int a, b;
+            Console.WriteLine("Enter the value of a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+               Console.WriteLine(a > b && a == 23);
+
+               Console.WriteLine(a > b && a == 23);
+
+
+
+
+        }
+    }
+}
